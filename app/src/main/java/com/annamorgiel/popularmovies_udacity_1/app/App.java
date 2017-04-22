e2b53bffa80d2e3bf30ac98e73246304c9118513
Anna Morgiel <morgiel@uni-potsdam.de>
2017-04-23 01:55:40 +0200

    Add internet permission and app App that creates Rest Client

--- app/src/main/java/com/annamorgiel/popularmovies_udacity_1/app/App.java
+++ app/src/main/java/com/annamorgiel/popularmovies_udacity_1/app/App.java
@@ -1,8 +1,27 @@
 package com.annamorgiel.popularmovies_udacity_1.app;
 
+import android.app.Application;
+
+import com.annamorgiel.popularmovies_udacity_1.Rest.RestClient;
+
 /**
  * Created by Anna Morgiel on 23.04.2017.
  */
 
-public class App {
+public class App extends Application {
+    private static RestClient restClient;
+
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+
+        restClient = new RestClient();
+    }
+
+    public static RestClient getRestClient()
+    {
+        return restClient;
+    }
+
 }
