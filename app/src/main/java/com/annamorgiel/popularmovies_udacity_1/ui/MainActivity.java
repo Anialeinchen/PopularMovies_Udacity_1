5765e25db7e01a779bb45d8ba9c8747d35b8150a
Anna Morgiel <morgiel@uni-potsdam.de>
2017-04-23 02:43:41 +0200

    movie adapter first draft

--- app/src/main/java/com/annamorgiel/popularmovies_udacity_1/ui/MainActivity.java
+++ app/src/main/java/com/annamorgiel/popularmovies_udacity_1/ui/MainActivity.java
@@ -2,14 +2,27 @@ package com.annamorgiel.popularmovies_udacity_1.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.annamorgiel.popularmovies_udacity_1.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView poster_rv;
+    private MovieAdapter adapter;
+    private static final int NUM_GRID_ITEM = 100;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //find RecyclerView and set GridLayoutManager to handle ViewHolders in a grid
+        poster_rv = (RecyclerView) findViewById(R.id.rv_movies);
+        GridLayoutManager layoutManager = new GridLayoutManager(this,NUM_GRID_ITEM);
+        poster_rv.setLayoutManager(layoutManager);
+
+        adapter = new MovieAdapter();
     }
 }
--- app/src/main/java/com/annamorgiel/popularmovies_udacity_1/ui/MovieAdapter.java
+++ app/src/main/java/com/annamorgiel/popularmovies_udacity_1/ui/MovieAdapter.java
@@ -1,8 +1,47 @@
 package com.annamorgiel.popularmovies_udacity_1.ui;
 
+import android.graphics.Movie;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+import java.util.List;
+
 /**
  * Created by Anna Morgiel on 23.04.2017.
  */
 
-public class MovieAdapter {
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
+
+    private static final String TAG = MovieAdapter.class.getSimpleName();
+
+    private List<Movie> movieList;
+
+    final private GridItemClickListener mOnClickListener;
+
+    public MovieAdapter(List<Movie> movieList, GridItemClickListener mOnClickListener) {
+        this.movieList = movieList;
+        this.mOnClickListener = mOnClickListener;
+    }
+
+    @Override
+    public MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(MovieViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return movieList.size();
+    }
+
+    public interface GridItemClickListener {
+        void onGridItemClick(int clickedItemIndex);
+    }
+
+    private class MovieViewHolder {
+    }
 }
