ace18bce31a7330ffad58b470f3e6610a697e474
Anna Morgiel <morgiel@uni-potsdam.de>
2017-05-03 19:39:49 +0200

    no @Parcel

--- app/src/main/java/com/annamorgiel/popularmovies_udacity_1/Rest/model/ApiResponse.java
+++ app/src/main/java/com/annamorgiel/popularmovies_udacity_1/Rest/model/ApiResponse.java
@@ -5,12 +5,9 @@ import android.os.Parcelable;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-import org.parceler.Parcel;
-
 import java.util.ArrayList;
 import java.util.List;
 
-@Parcel
 public class ApiResponse implements Parcelable{
 //changed total_movies to total results and also movies to results
     //giuhi
